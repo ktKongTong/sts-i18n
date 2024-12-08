ce09044bc9c3dcf2e53ab1652cffd35a64ba4e78
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-12-09 02:10:37 +0800

    initial commit simple-typesafe-i18n

--- apps/demo/src/_i18n/en.ts
+++ apps/demo/src/_i18n/en.ts
@@ -2,7 +2,7 @@ import { Tran } from "@ktfun/sts-i18n-react"
 
 const en = {
   count: (count: number) => `count is ${count}`,
-  'save-test-hmr' : 'Save for your next play!',
+  'save-test-hmr' : 'and save to test HMR',
   edit: 'Edit',
   'learn-more': "Click on the Vite and React logos to learn more"
 } satisfies Tran
